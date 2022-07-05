f44436e4567bd74f09980c5e5bf673b6efcda049
Mohammed Bheser <mbeshir26@gmail.com>
2022-07-05 15:07:49 +0300

    Task 0x0B v.2

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""Module 6-from_json_string.
-Returns an object (Python data structure)
-represented by a JSON string.
-"""
+from json import loads
 
 
-import json
-
-
-def from_json_string(my_str):
-    """Return the object represented my my_str.
-
-    Args:
-        - my_str: JSON string representation
-
-    Returns: corresponding object
-    """
-
-    return json.loads(my_str)
+def load_from_json_file(filename):
+    with open(filename, encoding='utf-8') as f:
+        return loads(f.read())
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""Module 7-save_to_json_file.
-Writes an Object to a text file,
-using a JSON representation.
-"""
+from os import path
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
 
-import json
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
 
-
-def save_to_json_file(my_obj, filename):
-    """Writes the representation of my_obj
-    to filename.
-
-    Args:
-        - my_obj: object to write
-        - filename: file to write into
-    """
-
-    with open(filename, 'w+') as f:
-        json.dump(my_obj, f)
+save_to_json_file(obj_json_file, 'add_item.json')
